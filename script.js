b915d7b9eb63d668ea933c4a82ae3411164db30e
Username070 <tarnas47@gmail.com>
2022-03-01 14:21:12 +0200

    TODO List

--- script.js
+++ script.js
@@ -38,22 +38,64 @@
 // }
 // DOM 6 dalis, 23 skaidre pabaiga
 
-var table = document.getElementById('table')
-document.getElementById('button').addEventListener('click', generateTable)
+// DOM 6 dalis, 24 skaidre
+// var table = document.getElementById('table')
+// document.getElementById('button').addEventListener('click', generateTable)
 
-function generateTable() {
-  var rows = document.getElementsByName('rows')
-  var columns = document.getElementsByName('columns')
+// function generateTable() {
+//   var rows = document.getElementsByName('rows')
+//   var columns = document.getElementsByName('columns')
 
-  for (var i = 0; i < rows[0].value; i++) {
-    var tr = document.createElement('tr')
-    table.appendChild(tr)
-    var currentTr = document.getElementsByTagName('tr')[i]
-    for (var j = 0; j < columns[0].value; j++) {
-      var td = document.createElement('td')
-      var tdText = document.createTextNode(`${i} . ${j}`)
-      td.appendChild(tdText)
-      currentTr.appendChild(td)
+//   for (var i = 0; i < rows[0].value; i++) {
+//     var tr = document.createElement('tr')
+//     table.appendChild(tr)
+//     var currentTr = document.getElementsByTagName('tr')[i]
+//     for (var j = 0; j < columns[0].value; j++) {
+//       var td = document.createElement('td')
+//       var tdText = document.createTextNode(`${i} . ${j}`)
+//       td.appendChild(tdText)
+//       currentTr.appendChild(td)
+//     }
+//   }
+// }
+// DOM 6 dalis, 24 skaidre
+
+var tbody = document.getElementsByTagName('tbody')[0]
+var idCount = 1
+
+document.getElementById('button').addEventListener('click', function () {
+  var subjectInfo = [
+    document.getElementById('subject'),
+    document.getElementById('priority'),
+    document.getElementById('date'),
+    document.getElementById('status'),
+  ]
+
+  for (var i = 0; i < 4; i++) {
+    if (subjectInfo[i].value.length == 0) {
+      throw new Error('Something went badly wrong!')
     }
   }
-}
+
+  var tr = document.createElement('tr')
+  tr.setAttribute('class', idCount)
+  tbody.appendChild(tr)
+
+  var idText = document.createTextNode(idCount)
+  var id = document.createElement('td')
+  id.appendChild(idText)
+  tr.appendChild(id)
+
+  var td
+  for (var i = 0; i < 4; i++) {
+    var tdText = document.createTextNode(subjectInfo[i].value)
+    td = document.createElement('td')
+    td.appendChild(tdText)
+    tr.appendChild(td)
+  }
+
+  const deleteIcon = document.createElement('td')
+  tr.appendChild(deleteIcon)
+  deleteIcon.innerHTML = `<button id="delete" name="${idCount}"><i class='fa-solid fa-x'></i></button>`
+  idCount++
+})
